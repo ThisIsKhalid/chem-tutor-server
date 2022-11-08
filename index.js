05d123081a0107d926436edf7234f5b4f6f7f541
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-08 16:32:30 +0600

    added mongodb

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uwm1xgh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.get('/', (req, res) => {
-    res.send('project is running');
-})
+async function run(){
+    
+}
+run().catch(err => console.log(err.name, err.message, err.stack));
+
+app.get("/", (req, res) => {
+  res.send("project is running");
+});
 
 app.listen(port, () => {
-    console.log(`server is running on ${port}`);
-})
+  console.log(`server is running on ${port}`);
+});
