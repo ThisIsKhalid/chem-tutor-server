98fa90d65021a2608666036fdf0489a6e43f14c5
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-08 20:31:13 +0600

    create dynamic route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,11 +19,26 @@ async function run(){
     try{
         const serviceCollection = client.db("chemTutor").collection("services");
 
+        app.get('/trending', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        });
+
         app.get('/services', async(req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            console.log(service);
+            res.send(service);
         })
     }
     finally{
