4ebc54b31cc2014295aa97d3d905fd97741b448e
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-11-10 18:24:17 +0600

    added successfully jwt

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const { application } = require("express");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+function verifyJWt(req, res, next) {
+  // console.log(req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next(); 
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uwm1xgh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,6 +37,14 @@ async function run() {
   try {
     const serviceCollection = client.db("chemTutor").collection("services");
     const reviewCollection = client.db("chemTutor").collection("reviews");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({token});
+    });
 
     // latest courses for home page
     app.get("/latest", async (req, res) => {
@@ -72,7 +97,13 @@ async function run() {
     });
 
     // personal reveiw load by email
-    app.get("/my_reviews", async (req, res) => {
+    app.get("/my_reviews",verifyJWt, async (req, res) => {
+      
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "unauthorized access" });
+      }
+
       let query = {};
       if (req.query.email) {
         query = {
